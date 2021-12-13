c35f2e49357f6ea91cccc7709958eea7b28b778c
HermanLoen <71780070+HermanLoen@users.noreply.github.com>
2021-12-13 11:58:30 +0100

    added icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,43 @@
 import React from 'react';
+import { MdHome } from "react-icons/md";
+import { MdContacts } from "react-icons/md";
+import { GoGlobe } from "react-icons/go";
+import { FaHandshake } from "react-icons/fa";
+import { MdPermIdentity } from "react-icons/md";
 
 const Header = () => {
     return (
+
         <div className={'navigation'}>
             <ul>
                 <li className={'menu-list'}>
                     <a href={'#'}>
-                        <span>icon</span>
-                        <span>text</span>
+                        <span><MdHome/></span>
+                        <span>Home</span>
                     </a>
                 </li>
                 <li className={'menu-list'}>
                     <a href={'#'}>
-                        <span>icon</span>
-                        <span>text</span>
+                        <span><GoGlobe/></span>
+                        <span>Our vision</span>
                     </a>
                 </li>
                 <li className={'menu-list'}>
                     <a href={'#'}>
-                        <span>icon</span>
-                        <span>text</span>
+                        <span><FaHandshake/></span>
+                        <span>Join us</span>
                     </a>
                 </li>
                 <li className={'menu-list'}>
                     <a href={'#'}>
-                        <span>icon</span>
-                        <span>text</span>
+                        <span><MdContacts/></span>
+                        <span>Contact</span>
+                    </a>
+                </li>
+                <li className={'menu-list'}>
+                    <a href={'#'}>
+                        <span><MdPermIdentity/></span>
+                        <span>Sign up</span>
                     </a>
                 </li>
             </ul>
