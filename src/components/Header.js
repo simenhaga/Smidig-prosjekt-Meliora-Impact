9f7965391b0d7257a0ec15ca360a9b57c2b2e3b4
Magnus Hodne <magnus@drobak.org>
2021-12-13 16:59:33 +0100

    Added bulma components to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,46 +18,52 @@ const Header = () => {
 
 
     return (
-        <div className={'header-container'}>
-            <div className={'logo-container'}>
-                <img src={logo}/>
+        <div className='container header-cont is-fluid'>
+            <div className='columns is-vcentered'>
+                <div className='column is-8 has-text-left'>
+                    <img style={{transform: 'scale(0.8)'}} src={logo}/>
+
+                </div>
+                <div className='column'>
+                    <div className={'navigation-container'}>
+                        <ul>
+                            <li className={'list active'}>
+                                <a href={'#'}>
+                                    <span className={'icon'}><MdHome/></span>
+                                    <span className={'text'}>Home</span>
+                                </a>
+                            </li>
+                            <li className={'list'}>
+                                <a href={'#'}>
+                                    <span className={'icon'}><GoGlobe/></span>
+                                    <span className={'text'}>Vision</span>
+                                </a>
+                            </li>
+                            <li className={'list'}>
+                                <a href={'#'}>
+                                    <span className={'icon'}><FaHandshake/></span>
+                                    <span className={'text'}>Join us</span>
+                                </a>
+                            </li>
+                            <li className={'list'}>
+                                <a href={'#'}>
+                                    <span className={'icon'}><MdContacts/></span>
+                                    <span className={'text'}>Contact</span>
+                                </a>
+                            </li>
+                            <li className={'list'}>
+                                <a href={'#'}>
+                                    <span className={'icon'}><MdPermIdentity/></span>
+                                    <span className={'text'}>Sign up</span>
+                                </a>
+                            </li>
+                            <div className={'indicator'}/>
+                        </ul>
+                    </div>
+                </div>
             </div>
-        <div className={'navigation-container'}>
-            <ul>
-                <li className={'list active'}>
-                    <a href={'#'}>
-                        <span className={'icon'}><MdHome/></span>
-                        <span className={'text'}>Home</span>
-                    </a>
-                </li>
-                <li className={'list'}>
-                    <a href={'#'}>
-                        <span className={'icon'}><GoGlobe/></span>
-                        <span className={'text'}>Vision</span>
-                    </a>
-                </li>
-                <li className={'list'}>
-                    <a href={'#'}>
-                        <span className={'icon'}><FaHandshake/></span>
-                        <span className={'text'}>Join us</span>
-                    </a>
-                </li>
-                <li className={'list'}>
-                    <a href={'#'}>
-                        <span className={'icon'}><MdContacts/></span>
-                        <span className={'text'}>Contact</span>
-                    </a>
-                </li>
-                <li className={'list'}>
-                    <a href={'#'}>
-                        <span className={'icon'}><MdPermIdentity/></span>
-                        <span className={'text'}>Sign up</span>
-                    </a>
-                </li>
-                <div className={'indicator'}/>
-            </ul>
         </div>
-        </div>
+
     );
 };
 
