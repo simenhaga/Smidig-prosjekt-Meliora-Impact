061ae4d0c60136c4bc0040fd1eea9d95a9bb7981
simenhaga <simen.haga@lyse.net>
2021-12-14 12:00:51 +0100

    Added trusted partners to Footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,26 +1,79 @@
 import React from 'react';
 import Logo from "../images/logo1.png"
+import Google from "../images/google.png"
+import Dnb from "../images/dnb.png"
+import Dnv from "../images/dnv.png"
+import Entur from "../images/entur.png"
+import Microsoft from "../images/microsoft.png"
+import "./Footer.css"
 
 
 const Footer = () => {
     return (
-        <footer className="footer" style={{width:"100%"}}>
-            <div className='columns'>
-                <div className='column'>
-                    <div className='columns is-vcentered'>
-                        <div className="column has-text-right">
-                            <img className='logo'
-                                src={Logo}
-                                alt={"Meliora Impact Logo"}
-                            />
+        <><section className="test has-background-black" style={{height:"100vh"}}>
+            <div className="section"></div>
+        </section>
+        
+        <section>
+            <footer className="footer" style={{ width: "100vw", padding:"0%" }}>
+                <section className="trustedPartners" style={{width: "100vw"}}>
+                    <div className='columns is-vcentered is-gapless'>
+                        <div className='column'></div>
+                        <div className='column is-size-6 has-text-right'>
+                            <p className='partnersText'>TRUSTED PARTNERS</p>
                         </div>
-                        <div className='column has-text-left is-size-7'>
-                            <p className='text'>© 2021 by Meliora Impact. Org.nr 926 638 564</p>
+                        <div className='column' style={{transform: "scale(0.5)"}}>
+                                <img className='dnbLogo'
+                                    src={Dnb}
+                                    alt={"Dnb Logo"} 
+                                />
+                        </div>
+                        <div className='column' style={{transform: "scale(0.5)"}}>
+                            <img className='dnvLogo'
+                                src={Dnv}
+                                alt={"Dnv Logo"} 
+                                />
+                        </div>
+                        <div className='column' style={{transform: "scale(0.5)"}}>
+                            <img className='googleLogo'
+                                src={Google}
+                                alt={"Google Logo"} 
+                                />
+                        </div>
+                        <div className='column' style={{transform: "scale(0.5)"}}>
+                            <img className='microsoftLogo'
+                                src={Microsoft}
+                                alt={"Microsoft Logo"} 
+                                />
+                        </div>
+                        <div className='column' style={{transform: "scale(0.5)"}}>
+                            <img className='enturLogo'
+                                src={Entur}
+                                alt={"Entur Logo"} 
+                                />
+                        </div>
+                        <div className='column'></div>
+                    </div>
+                    {/* <hr className='line' style={{color:"red", width: "50%"}}></hr> */}
+                    <div className='line' style={{ borderTop: "2px solid lightGrey ", width: "69%", marginLeft: "14.3%"}}></div>
+                </section>
+                <div className='columns'>
+                    <div className='column'>
+                        <div className='columns is-vcentered'>
+                            <div className='column has-text-right'>
+                                <img className='logo'
+                                    src={Logo}
+                                    alt={"Meliora Impact Logo"} 
+                                />
+                            </div>
+                            <div className='column has-text-left is-size-7'>
+                                <p className='text'>© 2021 by Meliora Impact. Org.nr 926 638 564</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </footer>
+            </footer>
+        </section></>
     );
 }
 
