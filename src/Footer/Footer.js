511b82856af0ef4a1fbbe8d91210e538cd215f36
simenhaga <simen.haga@lyse.net>
2021-12-14 14:28:44 +0100

    Removed empty columns

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -7,104 +7,96 @@ import Entur from "../images/entur.png"
 import Microsoft from "../images/microsoft.png"
 import "./Footer.css"
 
-
 const Footer = () => {
     return (
-        //PLACEHOLDER FOR BACKGROUND
-        <><section className="test has-background-black" style={{height:"100vh"}}>
-            <div className="section"></div>
-        </section>
-        
-        <section>
-            <footer className="footer" style={{ width: "100vw", padding:"0%" }}>
-                <section className="trustedPartners" style={{width: "100vw"}}>
-                    <div className='columns is-vcentered is-gapless'>
-                        <div className='column'></div>
-                        <div className='column is-size-6 has-text-right'>
-                            <p className='partnersText'>TRUSTED PARTNERS</p>
-                        </div>
-                        <div className='column' style={{transform: "scale(0.5)"}}>
-                                <img className='dnbLogo'
-                                    src={Dnb}
-                                    alt={"Dnb Logo"} 
-                                />
-                        </div>
-                        <div className='column' style={{transform: "scale(0.5)"}}>
-                            <img className='dnvLogo'
-                                src={Dnv}
-                                alt={"Dnv Logo"} 
-                                />
-                        </div>
-                        <div className='column' style={{transform: "scale(0.5)"}}>
-                            <img className='googleLogo'
-                                src={Google}
-                                alt={"Google Logo"} 
-                                />
-                        </div>
-                        <div className='column' style={{transform: "scale(0.5)"}}>
-                            <img className='microsoftLogo'
-                                src={Microsoft}
-                                alt={"Microsoft Logo"} 
-                                />
-                        </div>
-                        <div className='column' style={{transform: "scale(0.5)"}}>
-                            <img className='enturLogo'
-                                src={Entur}
-                                alt={"Entur Logo"} 
-                                />
-                        </div>
-                        <div className='column'></div>
-                    </div>
-                    <div className='line' style={{ borderTop: "2px solid lightGrey ", width: "69%", marginLeft: "14.3%", marginBottom: "3%"}}></div>
-                </section>
-
-                {/* INFORMATION */}
-                <section className='middleSection'>
-                    <div className='columns'>
-                    <div className='column'></div>
-                        <div className='column'>
-                            <div className='columns is-vcentered'>
-                                <div className='column has-text-left'>
-                                    <img className='logo'
-                                        src={Logo}
-                                        alt={"Meliora Impact Logo"} 
+        <footer className="footer" style={{ width: "100vw", padding:"0%" }}>
+                <div className='columns is-centered'>
+                    <div className='column is-9'>
+                        <div className='columns is-vcentered'>
+                            <div className='column is-size-6 has-text-right'>
+                                <p className='partnersText'>TRUSTED PARTNERS</p>
+                            </div>
+                            <div className='column' style={{transform: "scale(0.5)"}}>
+                                    <img className='dnbLogo'
+                                        src={Dnb}
+                                        alt={"Dnb Logo"} 
                                     />
+                            </div>
+                            <div className='column' style={{transform: "scale(0.5)"}}>
+                                <img className='dnvLogo'
+                                    src={Dnv}
+                                    alt={"Dnv Logo"} 
+                                    />
+                            </div>
+                            <div className='column' style={{transform: "scale(0.5)"}}>
+                                <img className='googleLogo'
+                                    src={Google}
+                                    alt={"Google Logo"} 
+                                    />
+                            </div>
+                            <div className='column' style={{transform: "scale(0.5)"}}>
+                                <img className='microsoftLogo'
+                                    src={Microsoft}
+                                    alt={"Microsoft Logo"} 
+                                    />
+                            </div>
+                            <div className='column' style={{transform: "scale(0.5)"}}>
+                                <img className='enturLogo'
+                                    src={Entur}
+                                    alt={"Entur Logo"} 
+                                    />
+                            </div>
+                        </div>
+
+                        <div className='columns'>
+                            <div className='column'>
+                                <div className='line' style={{ borderTop: "2px solid lightGrey ", marginBottom: "3%"}}></div>
+                            </div>
+                        </div>
+
+                        <div className='columns'>
+                            <div className='column'>
+                                <div className='columns is-vcentered'>
+                                    <div className='column has-text-left'>
+                                        <img className='logo'
+                                            src={Logo}
+                                            alt={"Meliora Impact Logo"} 
+                                        />
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className='column'></div>
-                        <div className='column'></div>
-                        <div className='column has-text-left'>
-                            <p className='has-text-weight-semibold'>SITE LINKS</p>
-                            <div className='siteList has-text-left' style={{listStyleType:"none"}}>
-                                <a href='#' >Home</a><br />
-                                <a href='#'>Vison</a><br />
-                                <a href='#'>Join Us</a><br />
-                                <a href='#'>Contact</a><br />
-                                <a href='#'>Sign Up</a>
+                            <div className='column'></div>
+                            <div className='column'></div>
+                            <div className='column has-text-left'>
+                                <p className='has-text-weight-semibold'>SITE LINKS</p>
+                                <div className='siteList has-text-left' style={{listStyleType:"none"}}>
+                                    <a href='#' >Home</a><br />
+                                    <a href='#'>Vison</a><br />
+                                    <a href='#'>Join Us</a><br />
+                                    <a href='#'>Contact</a><br />
+                                    <a href='#'>Sign Up</a>
+                                </div>
                             </div>
-                        </div>
-                        <div className='column'>
-                            <p className='has-text-weight-semibold'>SOCIALS</p>
-                            <div className='socialList has-text-left' style={{listStyleType:"none"}}>
-                                <a href='#' >Facebook</a><br />
-                                <a href='#'>Instagram</a><br />
-                                <a href='#'>LinkedIn</a><br />
+                            <div className='column'>
+                                <p className='has-text-weight-semibold'>SOCIALS</p>
+                                <div className='socialList has-text-left' style={{listStyleType:"none"}}>
+                                    <a href='#' >Facebook</a><br />
+                                    <a href='#'>Instagram</a><br />
+                                    <a href='#'>LinkedIn</a><br />
+                                </div>
                             </div>
+
                         </div>
-                        <div className='column'></div>
-                    </div>
-                </section>
+                        <div className='columns'>
+                            <div className='column has-text-centered is-size-7' style={{marginTop: "2%"}}>
+                                <p className='text'>© 2021 by Meliora Impact. Org.nr 926 638 564</p>
+                            </div>
 
-                {/* COPYRIGHTS */}
-                <section className='copyrightSection'>
-                    <div className='column has-text-centered is-size-7' style={{marginTop: "2%"}}>
-                        <p className='text'>© 2021 by Meliora Impact. Org.nr 926 638 564</p>
-                    </div>
-                </section>
+                        </div>
 
-            </footer>
-        </section></>
+                    </div>
+                </div>
+        </footer>
     );
 }
 
