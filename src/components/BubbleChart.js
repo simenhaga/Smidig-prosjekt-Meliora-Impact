c013d725b2677cd01ecca4985fc0b3ee1b71b447
HermanLoen <71780070+HermanLoen@users.noreply.github.com>
2021-12-15 14:11:15 +0100

    read from svg file

--- src/components/BubbleChart.js
+++ src/components/BubbleChart.js
@@ -1,9 +1,9 @@
 import React, {useEffect, useRef} from 'react'
 import * as d3 from 'd3'
+import data from './Meliora-categories.csv'
 
 const BubbleChart = () => {
 	const d3Chart = useRef() //Reference to the HTML object returned in this component
-
 	useEffect(() => {
 
 		//Dimensions for the chart container
@@ -16,25 +16,40 @@ const BubbleChart = () => {
 			.append('g')
 			.attr('transform', `translate(${height/2},${width/2})`)
 
-		d3.csv('./Meliora-categories.csv')
-			.then((data) => {
-				console.log(data)
+		d3.csv(data).then((data) => {
+			ready(data)
+			console.log(data)
 			}).catch((error) => {console.log('Failed to load data')})
 
-		const ready = (datapoints) => {
+		var simulation = d3.forceSimulation()
+
+		const ready = (data) => {
 			var circles = svg.selectAll(".category")
-				.data(datapoints)
+				.data(data)
 				.enter().append('circle')
 				.attr('class', 'category')
 				.attr('r', 10)
 				.attr('fill', 'lightblue')
+			/*simulation.data
+				.on('tick', ticked)
+			const ticked = () => {
+				circles
+					.attr("cx", function (d){
+						return d.x
+					})
+					.attr("cy", function (d){
+						return d.y
+					})
+			}*/
+
 		}
+
 
 	})
 
     return (
         <div id='wrapper'>
-			<svg ref={d3Chart}></svg>
+			<svg ref={d3Chart}/>
     	</div>
     )
 }
