0268c1b66461d74d1010269d16f4006c7a978251
Magnus Hodne <magnus@drobak.org>
2021-12-16 14:52:32 +0100

    Cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import 'bulma/css/bulma.min.css'
 import React from 'react'
-import { useState } from 'react'
 import { Link, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sandbox from "./components/Sandbox" //This element HAS to be created locally on your machine to avoid merge conflicts
 
@@ -8,42 +7,13 @@ import OldBubblePage from './pages/BubblePage';
 
 
 const App = () => {
-	const [tags, setTags] = useState([
-		{
-		  id: 1,
-		  title: "Women's Rights",
-		  selectionType: 0
-		},
-		{
-		  id: 2,
-		  title: 'Education',
-		  selectionType: 0
-		},
-		{
-		  id: 3,
-		  title: 'Clean Water',
-		  selectionType: 0
-		},
-		{
-		  id: 4,
-		  title: 'Human Rights',
-		  selectionType: 0
-		}
-	])
-	const toggleSelected = (id) => {
-		setTags(tags.map((tag) =>
-			tag.id === id ? {...tag, selectionType:
-			tag.selectionType === 2 ? 0 : ++tag.selectionType} : tag
-		))
-	}
-	
 	return (
 		<div className={"App"}>
 			<Router>
 				<Routes>
 					< Route exact path="/" element={<Sandbox/>}/>
 
-					< Route exact path="/bubbles" element={<OldBubblePage tags={tags} toggleSelected={toggleSelected}/>}/>
+					< Route exact path="/bubbles" element={<OldBubblePage/>}/>
 				</Routes>
 			</Router>
 		</div>
