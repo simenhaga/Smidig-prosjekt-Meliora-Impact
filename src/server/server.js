7df649cbba6220ddb18176dd237cfe9a2ade26f1
Magnus Hodne <magnus@drobak.org>
2022-03-04 17:26:39 +0100

    Basic server setup for express

---
+++ src/server/server.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import express from "express";
+import * as path from "path";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.static("../client/dist/"));
+
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Started on http://localhost:${server.address().port}`);
+});
