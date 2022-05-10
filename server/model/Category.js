867b5bd0abd350aaf77301105b2d9d2546375eba
HermanLoen <71780070+HermanLoen@users.noreply.github.com>
2022-05-10 13:55:36 +0200

    updated db model, created plant uml diagram for database structure

--- server/model/Category.js
+++ server/model/Category.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
-  type: [{ type: String }],
-  description: String,
+  title: String,
 });
 
 export const Category = mongoose.model("Category", categorySchema);
--- server/model/Company.js
+++ server/model/Company.js
@@ -3,6 +3,7 @@ import mongoose, { Schema } from "mongoose";
 const companySchema = new mongoose.Schema({
   name: { type: String, required: true },
   orgNr: { type: Number, required: true },
+  type: { type: String, enum: ["NON-PROFIT", "CUSTOMER"]},
   description: String,
   categoryList: [{ type: Schema.Types.ObjectId, ref: "Category" }],
 });
