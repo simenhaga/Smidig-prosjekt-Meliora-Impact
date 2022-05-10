cb56ac4056660e19c8462559092b6c147fe49857
simenhaga <simen.haga@lyse.net>
2022-05-10 11:06:02 +0200

    Implemented CategoryService.js

---
+++ server/service/CategoryService.js
@@ -0,0 +1,24 @@
+//import { Category } from "../model/Category.js";
+
+class Category {}
+
+const CategoryService = (function () {
+  return {
+    insert: async function (document) {
+      const category = new Category(document);
+      await category.save();
+      return category;
+    },
+    find: async function (query) {
+      return await Category.find(query);
+    },
+    delete: async function (query) {
+      return await Category.deleteOne(query);
+    },
+    update: async function (filter, query) {
+      return await Category.updateOne(filter, query);
+    },
+  };
+})();
+
+export { CategoryService };
