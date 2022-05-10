b6f5c8283cb125928aa52b9edcd2e1e254c7f1f5
HermanLoen <71780070+HermanLoen@users.noreply.github.com>
2022-05-10 13:57:29 +0200

    updated db model, created plant uml diagram for database structure

--- server/model/Company.js
+++ server/model/Company.js
@@ -3,15 +3,9 @@ import mongoose from "mongoose";
 const companySchema = new mongoose.Schema({
   name: { type: String, required: true },
   orgNr: { type: Number, required: true },
-  type: { type: String, enum: ["NON-PROFIT", "CUSTOMER"]},
+  type: { type: String, enum: ["NON-PROFIT", "CUSTOMER"], required: true, lowercase: true},
   description: String,
   categories: [{ type: mongoose.Schema.Types.ObjectId, ref: "Category" }],
-  type: {
-    type: String,
-    enum: ["non-profit", "customer"],
-    required: true,
-    lowercase: true,
-  },
 });
 
 export const Company = mongoose.model("Company", companySchema);
