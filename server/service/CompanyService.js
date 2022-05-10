48909c8536bbb2aa11e6dfc9ceadc5e77cc4a327
simenhaga <simen.haga@lyse.net>
2022-05-10 10:47:30 +0200

    Implemented CompanyService.js

---
+++ server/service/CompanyService.js
@@ -0,0 +1,24 @@
+//import { Company } from "../model/Company.js";
+
+class Company {}
+
+const CompanyService = (function () {
+  return {
+    insert: async function (document) {
+      const company = new Company(document);
+      await company.save();
+      return company;
+    },
+    find: async function (query) {
+      return await Company.find(query);
+    },
+    delete: async function (query) {
+      return await Company.deleteOne(query);
+    },
+    update: async function (filter, query) {
+      return await Company.updateOne(filter, query);
+    },
+  };
+})();
+
+export { CompanyService };
